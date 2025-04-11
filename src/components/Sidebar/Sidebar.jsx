79e3b61e92ffb4a17b0ba77b9850e3d5688b042f
hwyuaan <yuanitatri126@gmail.com>
2025-04-11 17:09:04 +0700

    update sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -8,52 +8,51 @@ import { useNavigate } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
-      <div className='container flex flex-row w-screen'>
-        {/* logo sidebar */}
-        <div className='container sidebar w-[270px] h-screen bg-[#ffffff] border-r border-gray-500 flex flex-col'>
-          <div className='logosidebar flex flex-row gap-1.5 mt-3 ml-3 '>
+      <div className='container flex flex-row w-full h-screen bg-white '>
+        {/* kiri */}
+        <div className='bg-gray-300 flex flex-col h-full w-[270px] '> 
+          <div className='flex flex-row gap-1.5 mt-3 ml-3 w-[194px] '>
             <img className='flex flex-row w-[80px] justify-center' src="Logo Mojokerto Sehat.svg" alt="imglogo" />
             <p className='font-[raleway] font-extrabold text-[#025F96] text-[25px] items-center '>MOJOKERTO SEHAT</p>
           </div>
-  
-            menu sidebar
-          <div className='menu sidebar flex flex-col gap-5 mt-10 ml-3'>
 
+          <div className='flex flex-col gap-5 mt-20 ml-3 '>
             <ul className='flex flex-col gap-5'>
-
               <li className='flex flex-row gap-2 items-center'>
                 <RiDashboardHorizontalFill className='w-10 h-10 text-[#004A76]'/>
-                <span className='font-[raleway] font-medium text-[#025F96] text-[20px]'>Dashbaord</span>
-
+                <span className='font-[raleway] font-semibold text-[#025F96] text-[19px]'>Dashbaord</span>
               </li>
-
               <li className='flex flex-row gap-2 items-center'>
                 <img src="icon-support.svg" alt="icon-support" />
-                <span className='font-[raleway] font-medium text-[#025F96] text-[20px]'>Konsultasi</span>
+                <span className='font-[raleway] font-semibold text-[#025F96] text-[19px]'>Konsultasi</span>
               </li>
-
               <li className='flex flex-row gap-2 items-center'>
                 <HiMiniUserGroup className='w-10 h-10 text-[#004A76]'/>
-                <span className='font-[raleway] font-medium text-[#025F96] text-[20px]'>Data Masyrakat </span>
-  
+                <span className='font-[raleway] font-semibold text-[#025F96] text-[19px]'>Data Masyrakat </span>
               </li>
-
               <li className='flex flex-row gap-2 items-center'>
                 <FaUserDoctor className='w-10 h-10 text-[#004A76]'/>
-                <span className='font-[raleway] font-medium text-[#025F96] text-[20px]'>Data Dokter</span>
-  
+                <span className='font-[raleway] font-semibold text-[#025F96] text-[19px]'>Data Dokter</span>
               </li>
-
               <li className='flex flex-row gap-2 items-center'>
                 <RiNewspaperFill className='w-10 h-10 text-[#004A76]' />
-                <span className='font-[raleway] font-medium text-[#025F96] text-[20px]'>Artikel</span>
+                <span className='font-[raleway] font-semibold text-[#025F96] text-[19px]'>Artikel</span>
               </li>
-             
             </ul>
-          </div>
+          </div> 
+
         </div>
+
+        {/* knan */}
+
+
       </div>
     )
 }
 
+
 export default Sidebar
+
+
+
+
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar/Sidebar'
+import { FaUser } from "react-icons/fa";
 
 function Dashboard() {
   return (
-    <div className='container '>
-      <Sidebar/>,
-      
+    <div className='container flex flex-row w-full h-screen bg-white'>
+      <div className='flex flex-row h-full w-[270px]'>
+        {/* kiri */}
+        <Sidebar />
+      </div>
+
+      {/* kanan */}
+      <div className='bg-white flex flex-col flex-1 pt-20'>
+        <div>
+            <button><FaUser className='text-3xl text-[#292D32] w-10 h-10' /></button>        
+        </div>
+        <div>
+          <img src="line style.svg" alt="" className='w-6/6' />
+        </div>
+      </div>
     </div>
+   
+    
+
+    
+
   )
 }
 
