4e929b802843cc99c7caec6168de044a26216bac
hwyuaan <yuanitatri126@gmail.com>
2025-04-13 20:24:26 +0700

    add hooks/root

--- src/pages/Splashscreen.jsx
+++ src/pages/Splashscreen.jsx
@@ -1,21 +1,33 @@
 import React from 'react'
-import { useEffect } from 'react'
+
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'
 
 
 
 const Splashscreen = () => {
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate(); 
+
+  useEffect(() => {
+      const timer = setTimeout(() => {
+        setLoading(false);
+        navigate('/login');
+      }, 2000);
+      return () => clearTimeout(timer);
+  }, [navigate]);
+
   return (
     <div className='loginapage logo flex flex-col w-screen h-screen justify-center items-center bg-[#F2F2F2] '>
       <div className='logo flex flex-col items-center pb-16'>
-          <img className='flex flex-row w-80 justify-center' src="Logo Mojokerto Sehat.svg" alt="imglogo" />
+          <img className='flex flex-row w-80 justify-center' src="/Logo Mojokerto Sehat.svg" alt="imglogo" />
           <p className='font-[raleway] font-extrabold text-[#025F96] text-[30px] items-center '>MOJOKERTO SEHAT</p>
           <div className=' flex flex-row '>
             <p className='text-[16px] text-[#438222] italic item-ends justify-end mr-0'>Dinas Kesehatan Kab.Mojokerto</p>
           </div>
       </div>
       <div className='flex flex-col items-center bottom-0 justify-center absolute'>
-        <img className='flex flex-row w-screen justify-center ' src="public/footer.svg" alt="imgfooter" />
+        <img className='flex flex-row w-screen justify-center ' src="/footer.svg" alt="imgfooter" />
       </div>
     </div>
 
