9c4cdbe4bd595d64bf2cf67226890d7994d516c1
hwyuaan <yuanitatri126@gmail.com>
2025-04-15 11:29:45 +0700

    update error:main,sidebar,dashboard

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,57 +1,91 @@
 import React from 'react'
+import { useState } from 'react';
+
+// icon
 import { RiDashboardHorizontalFill } from "react-icons/ri";
 import { HiMiniUserGroup } from "react-icons/hi2";
 import { FaUserDoctor } from "react-icons/fa6";
 import { RiNewspaperFill } from "react-icons/ri";
-import { useNavigate } from 'react-router-dom';
+import { BsBoxArrowLeft } from "react-icons/bs";
+import { BsBoxArrowRight } from "react-icons/bs";
+import { CiAlignLeft } from "react-icons/ci";
+// import { useNavigate } from 'react-router-dom';
 
 
-const Sidebar = () => {
+
+export default function Sidebar({icon,text,active,alert}) {
+  const [open,setOpen] = useState(true);
   return (
-      <div className='container flex flex-row w-full h-screen bg-white '>
+      <div className='fixed flex flex-row h-screen bg-white'>
         {/* kiri */}
-        <div className='bg-gray-300 flex flex-col h-full w-[270px] '> 
-          <div className='flex flex-row gap-1.5 mt-3 ml-3 w-[194px] '>
-            <img className='flex flex-row w-[80px] justify-center' src="Logo Mojokerto Sehat.svg" alt="imglogo" />
-            <p className='font-[raleway] font-extrabold text-[#025F96] text-[25px] items-center '>MOJOKERTO SEHAT</p>
+        <div className={`flex flex-col h-full border-r border-[#F0F0F0] drop-shadow-md ${open ? "w-[250px]" : "w-[80px]"} transition-all duration-300 justify-between`}> 
+          <div className='flex flex-row gap-1.5 mt-3 ml-3 items-center relative '>
+            <div className='flex flex-row gap-2  w-[300px] overflow-hidden'>
+                <img className={`transition-all duration-500 relative ${open ? "w-[80px]" : "w-[50px]"}`}
+                src="/Logo Mojokerto Sehat.svg" 
+                alt="imglogo" />
+                <p className={`font-[raleway] font-extrabold text-[#025F96] text-[20px] items-center transition-opacity duration-300 ${open ? 'opacity-100' : 'opacity-0'}`}>MOJOKERTO SEHAT</p>
+            </div>
           </div>
 
+          
+          {/* isi sidebar */}
           <div className='flex flex-col gap-5 mt-20 ml-3 '>
-            <ul className='flex flex-col gap-5'>
-              <li className='flex flex-row gap-2 items-center'>
+            <ul className='flex flex-col gap-5 relative'>
+              <li className='flex flex-row gap-2 items-center' id='dashboard'>
                 <RiDashboardHorizontalFill className='w-10 h-10 text-[#004A76]'/>
-                <span className='font-[raleway] font-semibold text-[#025F96] text-[19px]'>Dashbaord</span>
+                <div
+                  className={`transition-all duration-300 overflow-hidden ${open ? 'max-w-[180px] opacity-100 ml-1' : 'max-w-0 opacity-0'}`}>
+                  <span className="font-[raleway] font-semibold text-[#025F96] text-[19px]">Dashboard</span>
+                </div>
               </li>
+
               <li className='flex flex-row gap-2 items-center'>
                 <img src="icon-support.svg" alt="icon-support" />
-                <span className='font-[raleway] font-semibold text-[#025F96] text-[19px]'>Konsultasi</span>
+                <div
+                  className={`transition-all duration-300 overflow-hidden ${open ? 'max-w-[180px] opacity-100 ml-1' : 'max-w-0 opacity-0'}`}>
+                  <span className="font-[raleway] font-semibold text-[#025F96] text-[19px]">Konsultasi</span>
+                </div>
               </li>
+
               <li className='flex flex-row gap-2 items-center'>
                 <HiMiniUserGroup className='w-10 h-10 text-[#004A76]'/>
-                <span className='font-[raleway] font-semibold text-[#025F96] text-[19px]'>Data Masyrakat </span>
+                <div
+                  className={`transition-all duration-300 overflow-hidden ${open ? 'max-w-[180px] opacity-100 ml-1' : 'max-w-0 opacity-0'}`}>
+                  <span className="font-[raleway] font-semibold text-[#025F96] text-[19px]">Data Masyrakat</span>
+                </div>
               </li>
+
               <li className='flex flex-row gap-2 items-center'>
                 <FaUserDoctor className='w-10 h-10 text-[#004A76]'/>
-                <span className='font-[raleway] font-semibold text-[#025F96] text-[19px]'>Data Dokter</span>
+                <div
+                  className={`transition-all duration-300 overflow-hidden ${open ? 'max-w-[180px] opacity-100 ml-1' : 'max-w-0 opacity-0'}`}>
+                  <span className="font-[raleway] font-semibold text-[#025F96] text-[19px]">Data Dokter</span>
+                </div>
               </li>
+              
               <li className='flex flex-row gap-2 items-center'>
                 <RiNewspaperFill className='w-10 h-10 text-[#004A76]' />
-                <span className='font-[raleway] font-semibold text-[#025F96] text-[19px]'>Artikel</span>
+                <div
+                  className={`transition-all duration-300 overflow-hidden ${open ? 'max-w-[180px] opacity-100 ml-1' : 'max-w-0 opacity-0'}`}>
+                  <span className="font-[raleway] font-semibold text-[#025F96] text-[19px]">Artikel</span>
+                </div>
               </li>
             </ul>
+            <div className='p-1 pt-3'>
+              <button onClick={() => setOpen((curr) => !curr) } 
+                  className='bg-[#004a76]/50 rounded-lg w-10 shadow-2xl cursor-pointer transition-all hover:bg-[#004A76] hover:text-white'>
+                  <CiAlignLeft className='w-10 h-10 text-[white]'/>
+              </button>
+            </div>
           </div> 
-
+          
         </div>
 
-        {/* knan */}
-
-
+       
       </div>
     )
 }
-
-
-export default Sidebar
 
 
 
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,24 +1,73 @@
 import React from 'react'
+import { useState, useRef, useEffect } from 'react';
+
 import Sidebar from '../components/Sidebar/Sidebar'
-import { FaUser } from "react-icons/fa";
+import { FaCircleUser } from "react-icons/fa6";
+
+
+import "cally"
+
+
 
 function Dashboard() {
+
+    const [isOpen, setIsOpen] = useState(false);
+  
+    const toggleDropdown = () => setIsOpen(!isOpen);
+
   return (
-    <div className='container flex flex-row w-full h-screen bg-white'>
-      <div className='flex flex-row h-full w-[270px]'>
-        {/* kiri */}
-        <Sidebar />
+    <div className='flex flex-row '>
+      {/* kiri */}
+      <div className='w-1/6'>
+      <Sidebar />
       </div>
 
       {/* kanan */}
-      <div className='bg-white flex flex-col flex-1 pt-20'>
-        <div>
-            <button><FaUser className='text-3xl text-[#292D32] w-10 h-10' /></button>        
+      <main className='w-5/6 min-h-screen bg-white flex flex-col pl-10 pr-5'>
+        <div className='flex flex-row gird-2 items-center justify-between relative pt-3'>
+          <p className='text-[25px] font-[raleway] font-bold text-[#025f96]'>Dashboard</p>
+          <button onClick={toggleDropdown} className="flex items-center space-x-2 focus:outline-none cursor-pointer">
+            <FaCircleUser className='w-[30px] h-[30px] text-[#292D32]'>  </FaCircleUser>
+            <div>
+            {isOpen && (
+              <div className="absolute right-0 mt-2 w-44 origin-top-right rounded-md shadow-lg bg-white ring-1 ring-blue ring-opacity-3 z-50 ">
+                
+                <div className="py-1">
+                  <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Profil Admin</a>
+                  <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"> Log Out</a>
+                </div>
+
+              </div>
+            )}
+            </div>
+          </button>
+        </div>
+        <img src="line style.svg" alt="" />
+
+
+        <div className='flex flex-row gap-5 mt-5'>
+          <div className='w-[386px] h-[200px] bg-[#38B6FE]/28 rounded-lg shadow-2xl'>
+           <div className="z-10">
+              <h2 className="text-lg font-bold text-gray-800">Good Morning , Admin.</h2>
+              <p className="text-sm text-gray-700">Selamat datang di Website Mojokerto Sehat</p>
+            </div>
+           <img src="blob_img_comp.svg" alt="" className='justify-beetwen h-[165px] w-[300px]'/>
+           <img src="img_org.svg" alt="" className='relative w-[350px] object-fill ' />
+
+          </div>
+          <div class="relative max-w-sm">
+            <calendar-date class="cally bg-base-100 border border-base-300 shadow-lg rounded-box">
+              <svg aria-label="Previous" className="fill-current size-4" slot="previous" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M15.75 19.5 8.25 12l7.5-7.5"></path></svg>
+              <svg aria-label="Next" className="fill-current size-4" slot="next" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="m8.25 4.5 7.5 7.5-7.5 7.5"></path></svg>
+              <calendar-month></calendar-month>
+            </calendar-date>
+          </div>
+
         </div>
         <div>
-          <img src="line style.svg" alt="" className='w-6/6' />
+          ndjsjd
         </div>
-      </div>
+      </main>
     </div>
    
     
