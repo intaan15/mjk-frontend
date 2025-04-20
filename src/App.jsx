df74bb976cd0addb67af834baf6d1b8727b43a8c
hwyuaan <yuanitatri126@gmail.com>
2025-04-21 06:45:16 +0700

    Update: App,Index, Main

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,60 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import './App.css'
-import Splashscreen from './pages/Splashscreen'
-import Loginakun from './pages/Loginakun'
-import Dashboard from './pages/Dashboard'
-import Artikel from './pages/Artikel'
-import Konsultasi from './pages/Konsultasi'
-import Dokter from './pages/Dokter'
+import  './app.css'
+import Layout from './pages/MainLayOut/Layout'
+import Splashscreen from './pages/root/Splashscreen'
+import Loginakun from './pages/root/Loginakun'
+import Dashboard from './pages/Dashboard/Dashboard'
+import Artikel from './pages/Artikel/Artikel'
+import Konsultasi from './pages/Konsultasi/Konsultasi'
+import Dokter from './pages/Dokter/Dokter'
 import Bar from './components/Bar/Bar'
-import Jadwal from './pages/Jadwal'
+import Jadwal from './pages/Dokter/Jadwal'
+import DataMasyarakat from './pages/Masyarakat/DataMasyarakat';
+import Verifikasi from './pages/Masyarakat/Verifikasi'
 
 
 function App() {
  return(
-  <Splashscreen/>,
-  <Loginakun/>,
-  <Dashboard/>,
-  <Artikel/>,
-  <Konsultasi/>,
-  <Dokter/>,
-  <Bar/>,
-  <Jadwal/>
+    // <Router>
+    //     <Routes>
+    //         <Route path="/" element={<Splashscreen />} />
+    //         <Route path="/login" element={<Loginakun />} />
+    //         <Route path="/dashboardadmin" element={<Dashboard />} />
+    //         <Route path="/artikel" element={<Artikel />} />
+    //         <Route path="/konsultasi" element={<Konsultasi />} />
+    //         <Route path="/dokter/datadokter" element={<Dokter />} />
+    //         <Route path="/dokter/jadwal" element={<Jadwal />} />
+    //     </Routes>
+
+    // </Router>
+    <Router >
+        <Routes>
+            <Route path="/" element={<Splashscreen />} />
+            <Route path="/login" element={<Loginakun />} />
+            <Route path="/"  element={<Layout />}>
+                <Route path="/dashboardadmin" element={<Dashboard />} />
+                <Route path="/artikel" element={<Artikel />} />
+                <Route path="/konsultasi" element={<Konsultasi />} />
+                <Route path="/dokter/datadokter" element={<Dokter />} />
+                <Route path="/dokter/jadwal" element={<Jadwal />} />
+                <Route path="/masyarakat/data" element={<DataMasyarakat />} />
+                <Route path="/masyarakat/verifikasi" element={<Verifikasi />} />
+               
+            </Route>
+
+           
+        </Routes>
+    </Router>
+    
+
+
  )
 }
 
 export default App;
+
+
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,38 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-import App from './App.jsx'
-import './index.css'
-import Loginakun from './pages/Loginakun.jsx'
-import Splashscreen from './pages/Splashscreen.jsx'
-import Dashboard from './pages/Dashboard.jsx'
-import Artikel from './pages/Artikel.jsx'
-import Konsultasi from './pages/Konsultasi.jsx'
-import Dokter from './pages/Dokter.jsx'
-import Jadwal from './pages/Jadwal.jsx'
-
-
-
-const Router = createBrowserRouter([
-  { path: '/', 
-    element: <Splashscreen/>, },
-  { path: '/login', 
-    element: <Loginakun/>, },
-  { path: '/dashboardadmin', 
-    element: <Dashboard/>, },
-  { path: '/artikel', 
-    element: <Artikel/>, },
-  { path: '/Konsultasi', 
-    element: <Konsultasi/>, },
-  { path: '/dokter/datadokter', 
-    element: <Dokter/>, },
-  { path: '/dokter/jadwal', 
-    element: <Jadwal/>, }
-])
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={Router}/>
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
