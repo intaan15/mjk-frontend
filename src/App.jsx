bb45437b92ba162a2681c575c9af3a637660319b
rayhanizzudd <rayhannavis11@gmail.com>
2025-05-07 10:53:33 +0700

    add : private route

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,50 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import axios from 'axios'
+import axios from "axios";
 
-import './App.css'
-import  './app.css'
-import Layout from './pages/MainLayOut/Layout'
-import Splashscreen from './pages/root/Splashscreen'
-import Loginakun from './pages/root/Loginakun'
-import Dashboard from './pages/Dashboard/Dashboard'
-import Artikel from './pages/Artikel/Artikel'
-import Konsultasi from './pages/Konsultasi/Konsultasi'
-import Dokter from './pages/Dokter/Dokter'
-import Bar from './components/Bar/Bar'
-import Jadwal from './pages/Dokter/Jadwal'
-import DataMasyarakat from './pages/Masyarakat/DataMasyarakat';
-import Verifikasi from './pages/Masyarakat/Verifikasi'
-import Modal from './components/Modal'
-
+import "./App.css";
+import "./app.css";
+import Layout from "./pages/MainLayOut/Layout";
+import Splashscreen from "./pages/root/Splashscreen";
+import Loginakun from "./pages/root/Loginakun";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Artikel from "./pages/Artikel/Artikel";
+import Konsultasi from "./pages/Konsultasi/Konsultasi";
+import Dokter from "./pages/Dokter/Dokter";
+import Bar from "./components/Bar/Bar";
+import Jadwal from "./pages/Dokter/Jadwal";
+import DataMasyarakat from "./pages/Masyarakat/DataMasyarakat";
+import Verifikasi from "./pages/Masyarakat/Verifikasi";
+import Modal from "./components/Modal";
+import PrivateRoute from "./components/Privateroutes";
 
 function App() {
-    
- return(
-    
-    <Router >
-        <Routes>
-            <Route path="/" element={<Splashscreen />} />
-            <Route path="/login" element={<Loginakun />} />
-            <Route path="/modal" element={<Modal />} />
-            
-            <Route path="/"  element={<Layout />}>
-                <Route path="/dashboardadmin" element={<Dashboard />} />
-                <Route path="/artikel" element={<Artikel />} />
-                <Route path="/konsultasi" element={<Konsultasi />} />
-                <Route path="/dokter/datadokter" element={<Dokter />} />
-                <Route path="/dokter/jadwal" element={<Jadwal />} />
-                <Route path="/masyarakat/data" element={<DataMasyarakat />} />
-                <Route path="/masyarakat/verifikasi" element={<Verifikasi />} />
-               
-            </Route>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Splashscreen />} />
+        <Route path="/login" element={<Loginakun />} />
+        <Route path="/modal" element={<Modal />} />
 
-           
-        </Routes>
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Layout />
+            </PrivateRoute>
+          }
+        >
+          <Route path="/dashboardadmin" element={<Dashboard />} />
+          <Route path="/artikel" element={<Artikel />} />
+          <Route path="/konsultasi" element={<Konsultasi />} />
+          <Route path="/dokter/datadokter" element={<Dokter />} />
+          <Route path="/dokter/jadwal" element={<Jadwal />} />
+          <Route path="/masyarakat/data" element={<DataMasyarakat />} />
+          <Route path="/masyarakat/verifikasi" element={<Verifikasi />} />
+        </Route>
+      </Routes>
     </Router>
-    
-
-
- )
+  );
 }
 
 export default App;
-
-
-
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -14,6 +14,7 @@ import { TiUser } from "react-icons/ti";
 import { BsFillBarChartFill } from "react-icons/bs";
 import { GrArticle } from "react-icons/gr";
 import { FaUserClock } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 
 // kenapa diluar func? agar tidak boros memori,dan efisien apabila terdapat elemen dashboard yg dirender ulang
@@ -74,126 +75,166 @@ function Dashboard() {
     });
 
 
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+      // Hapus token dari localStorage
+      localStorage.removeItem("token");
+
+      // Redirect ke halaman login
+      navigate("/login");
+    };
+
 
 
   return (
-    <div className='flex flex-row '>
-
+    <div className="flex flex-row ">
       {/* navbar */}
-      <main className=' w-full md:5/6 flex flex-col pl-18 pr-5 gap-1 bg-gray-50 '>
-        <div className='flex flex-row grid-2 items-center justify-between  pt-2'>
-          <p className='text-[25px] font-[Nunito Sans] font-bold text-[#004A76]'>Dashboard</p>
-          <button onClick={toggleDropdown} className="flex items-center space-x-2 focus:outline-none cursor-pointer">
-            <TiUser className='w-[40px] h-[40px] text-[#292D32]'> </TiUser>
+      <main className=" w-full md:5/6 flex flex-col pl-18 pr-5 gap-1 bg-gray-50 ">
+        <div className="flex flex-row grid-2 items-center justify-between  pt-2">
+          <p className="text-[25px] font-[Nunito Sans] font-bold text-[#004A76]">
+            Dashboard
+          </p>
+          <button
+            onClick={toggleDropdown}
+            className="flex items-center space-x-2 focus:outline-none cursor-pointer"
+          >
+            <TiUser className="w-[40px] h-[40px] text-[#292D32]"> </TiUser>
             <div>
-            {isOpen && (
-              <div className="absolute right-0 mt-2 w-44 origin-top-right rounded-md shadow-lg bg-white ring-1  ring-opacity-3 z-50 ">
-                
-                <div className="py-1">
-                  <a href="#" className="block py-2 text-sm text-gray-700 hover:bg-gray-100 ">Administrator   </a>
-                  <a href="/" className="block py-2 text-sm text-gray-700 hover:bg-gray-100"> Log Out</a>
+              {isOpen && (
+                <div className="absolute right-0 mt-2 w-44 origin-top-right rounded-md shadow-lg bg-white ring-1  ring-opacity-3 z-50 ">
+                  <div className="py-1">
+                    <a
+                      href="#"
+                      className="block py-2 text-sm text-gray-700 hover:bg-gray-100 "
+                    >
+                      Administrator{" "}
+                    </a>
+                    <a
+                      href="#"
+                      onClick={handleLogout}
+                      className="block py-2 text-sm text-gray-700 hover:bg-gray-100"
+                    >
+                      {" "}
+                      Log Out
+                    </a>
+                  </div>
                 </div>
-
-              </div>
-            )}
+              )}
             </div>
           </button>
         </div>
         <img src="line style.svg" alt="" />
 
-        
         {/* col 1 */}
-        <div className='flex flex-row pl-1'>
-          <div className=''>
+        <div className="flex flex-row pl-1">
+          <div className="">
             <div className="font-bold text-white pl-3 absolute flex flex-col pt-5  gap-16   ">
-              <div className='justify-center items-center pt-2'>
-                <Calendar className="w-5 h-5"/>
+              <div className="justify-center items-center pt-2">
+                <Calendar className="w-5 h-5" />
               </div>
-              <div className='items-end relative '>
-                <h2 className="font-[raleway] text-[28px] font-bold text-white">Hi, Admin</h2>
-                <p className="font-[Nunito Sans] italic text-[20px] text-white font-medium ">Selamat datang di Website Mojokerto Sehat</p>
-              </div>   
+              <div className="items-end relative ">
+                <h2 className="font-[raleway] text-[28px] font-bold text-white">
+                  Hi, Admin
+                </h2>
+                <p className="font-[Nunito Sans] italic text-[20px] text-white font-medium ">
+                  Selamat datang di Website Mojokerto Sehat
+                </p>
+              </div>
             </div>
-            <img src="img_org.svg" alt="" className='' />
+            <img src="img_org.svg" alt="" className="" />
           </div>
         </div>
-          
+
         {/* tengah/col2 */}
-        <div className='bg-[#004A76] h-[80px] rounded-xl flex flex-row justify-center items-center shadow-md'>
+        <div className="bg-[#004A76] h-[80px] rounded-xl flex flex-row justify-center items-center shadow-md">
           <StatBox
-            icon={<BsFillBarChartFill className='w-[30px] h-[30px] text-white font-[Nunito Sans]' />}
+            icon={
+              <BsFillBarChartFill className="w-[30px] h-[30px] text-white font-[Nunito Sans]" />
+            }
             title="Jumlah Pengguna"
             value={stats.jumlahPengguna}
           />
           <StatBox
-            icon={<GrArticle className='w-[30px] h-[30px] text-white' />}
+            icon={<GrArticle className="w-[30px] h-[30px] text-white" />}
             title="Artikel Publish"
             value={stats.artikelPublish}
           />
           <StatBox
-            icon={<FaUserClock className='w-[30px] h-[30px] text-white' />}
+            icon={<FaUserClock className="w-[30px] h-[30px] text-white" />}
             title="Verifikasi Pengguna"
             value={stats.filteredData}
           />
         </div>
-        
 
-        
-        
         {/* col 3 */}
-          <div className='w-full h-auto rounded-3xl flex flex-col gap-3'>
-            <div>
-              <p className='font-[Nunito Sans] text-[20px] font-bold pl-5  text-[#025f96] justify-between '>Log Aktivitas Pengguna </p>
-            </div>
+        <div className="w-full h-auto rounded-3xl flex flex-col gap-3">
+          <div>
+            <p className="font-[Nunito Sans] text-[20px] font-bold pl-5  text-[#025f96] justify-between ">
+              Log Aktivitas Pengguna{" "}
+            </p>
+          </div>
 
-            <div className='flex flex-row gird-rows-2 w-full gap-8 justify-between'>
-
-              <div className='rounded-xl flex flex-row justify-center items-center'>
-                <div className='flex flex-row '>
-                  <div className="grid grid-cols-2 gap-1 bg-[#D9D9D9] h-auto w-[430px] justify-center  rounded-xl p-4">
-                    <div>
-                      <h3 className="font-bold text-[16px] text-black underline">Konsultasi</h3>
-                      <p className="text-[15px] text-black">35 Konsultasi</p>
-                    </div>
-                    <div>
-                      <h3 className="font-bold text-[16px] text-black underline">Akun Baru</h3>
-                      <p className="text-[15px] text-black">35 Akun Baru</p>
-                    </div>
-                    <div>
-                      <h3 className="font-bold text-[16px] text-black underline">Dokter Aktif</h3>
-                      <p className="text-[15px] text-black">35 Akun Dokter</p>
-                    </div>
-                    <div>
-                      <h3 className="font-bold text-[16px] text-black underline">Artikel Publish</h3>
-                      <p className="text-[15px] text-black">35 Artikel</p>
-                    </div>
+          <div className="flex flex-row gird-rows-2 w-full gap-8 justify-between">
+            <div className="rounded-xl flex flex-row justify-center items-center">
+              <div className="flex flex-row ">
+                <div className="grid grid-cols-2 gap-1 bg-[#D9D9D9] h-auto w-[430px] justify-center  rounded-xl p-4">
+                  <div>
+                    <h3 className="font-bold text-[16px] text-black underline">
+                      Konsultasi
+                    </h3>
+                    <p className="text-[15px] text-black">35 Konsultasi</p>
+                  </div>
+                  <div>
+                    <h3 className="font-bold text-[16px] text-black underline">
+                      Akun Baru
+                    </h3>
+                    <p className="text-[15px] text-black">35 Akun Baru</p>
+                  </div>
+                  <div>
+                    <h3 className="font-bold text-[16px] text-black underline">
+                      Dokter Aktif
+                    </h3>
+                    <p className="text-[15px] text-black">35 Akun Dokter</p>
+                  </div>
+                  <div>
+                    <h3 className="font-bold text-[16px] text-black underline">
+                      Artikel Publish
+                    </h3>
+                    <p className="text-[15px] text-black">35 Artikel</p>
                   </div>
                 </div>
               </div>
+            </div>
 
-              <div className='bg-[#D9D9D9] w-[530px] rounded-xl flex flex-row justify-center items-center'>
-                <Bar />
-                <div>
+            <div className="bg-[#D9D9D9] w-[530px] rounded-xl flex flex-row justify-center items-center">
+              <Bar />
+              <div>
                 <div className="text-sm space-y-1">
-                  <p><span className="inline-block w-3 h-3 rounded-full bg-pink-400 mr-2"></span>Konsultasi (300)</p>
-                  <p><span className="inline-block w-3 h-3 rounded-full bg-teal-400 mr-2"></span>Masyarakat (1200)</p>
-                  <p><span className="inline-block w-3 h-3 rounded-full bg-blue-400 mr-2"></span>Dokter (150)</p>
-                  <p><span className="inline-block w-3 h-3 rounded-full bg-yellow-400 mr-2"></span>Artikel (500)</p>
-                </div>
+                  <p>
+                    <span className="inline-block w-3 h-3 rounded-full bg-pink-400 mr-2"></span>
+                    Konsultasi (300)
+                  </p>
+                  <p>
+                    <span className="inline-block w-3 h-3 rounded-full bg-teal-400 mr-2"></span>
+                    Masyarakat (1200)
+                  </p>
+                  <p>
+                    <span className="inline-block w-3 h-3 rounded-full bg-blue-400 mr-2"></span>
+                    Dokter (150)
+                  </p>
+                  <p>
+                    <span className="inline-block w-3 h-3 rounded-full bg-yellow-400 mr-2"></span>
+                    Artikel (500)
+                  </p>
                 </div>
               </div>
             </div>
-        
-             
+          </div>
         </div>
       </main>
     </div>
-   
-    
-
-    
-
-  )
+  );
 }
 
 export default Dashboard
