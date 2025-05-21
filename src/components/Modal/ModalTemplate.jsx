6d9f5aa17edff995ae69620e4b744c21167c30dd
rayhanizzudd <rayhannavis11@gmail.com>
2025-05-21 11:28:15 +0700

    update : template modal

--- src/components/Modal/ModalTemplate.jsx
+++ src/components/Modal/ModalTemplate.jsx
@@ -1,27 +1,40 @@
 import { useEffect, useState } from "react";
-export default function Modal({ open, onClose, children }) {  
-  if (!open) return null;
 
+export default function Modal({ open, onClose, children }) {
+  const [isVisible, setIsVisible] = useState(false);
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    if (open) {
+      setIsVisible(true);
+      setTimeout(() => setAnimate(true), 15);
+    } else {
+      setAnimate(false);
+      setIsVisible(false); 
+    }
+  }, [open]);
+
+  if (!isVisible) return null;
 
   return (
-    // backdrop
     <div
       onClick={onClose}
-      className={`fixed inset-0 flex justify-center items-center transition-colors z-50
-        ${open ? "visible bg-black/40" : "invisible"}
-      `}
+      className={`fixed inset-0 z-50 flex justify-center items-center bg-black/40 transition-opacity duration-300 ${
+        animate ? "opacity-100" : "opacity-0"
+      }`}
     >
       <div
         onClick={(e) => e.stopPropagation()}
-        className={`relative bg-white rounded-lg shadow transition-all w-3/5
-          ${open ? "scale-100 opacity-100" : "scale-125 opacity-0"}
-        `}
+        className={`relative bg-white rounded-lg shadow-md w-3/5 transition-opacity duration-300 ${
+          animate ? "opacity-100" : "opacity-0"
+        }`}
       >
         <div className="p-6 overflow-y-auto max-h-[80vh] scrollbar-hide">
           <button
             onClick={onClose}
             className="absolute top-2 right-2 p-1 rounded-lg text-gray-400 bg-white hover:bg-gray-50 hover:text-gray-600"
-          ></button>
+          >
+          </button>
           {children}
         </div>
       </div>
