2d9cbea7d85b1ce46cd068f26a452de901f562c8
hwyuaan <yuanitatri126@gmail.com>
2025-06-21 14:52:53 +0700

    fix use logout: confitmation

--- src/components/_hooksPages/useLogout.jsx
+++ src/components/_hooksPages/useLogout.jsx
@@ -1,11 +1,26 @@
 import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const useAuth = () => {
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    localStorage.removeItem("token");
-    navigate("/login");
+    // Hapus token dari localStorage
+    Swal.fire({
+      title: "Logout",
+      text: "Apakah Anda yakin ingin keluar?",
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonText: "Ya",
+      confirmButtonColor: "#004A76",
+      cancelButtonText: "Tidak"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        localStorage.removeItem("token");
+        navigate("/login");
+      }
+
+    });
   };
 
   return {
